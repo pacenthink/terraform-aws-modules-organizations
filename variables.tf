1485db84d030538ed47085b666c71ba45f817c56
Arnav <arnavchachra.ac@gmail.com>
2023-12-25 21:09:14 +0530

    fix: made input variabels as required

--- variables.tf
+++ variables.tf
@@ -1,10 +1,8 @@
 variable "account_name" {
   description = "Name of the new AWS org account"
   type        = string
-  default     = "devops"
 }
 variable "account_email" {
   description = "Email of the new AWS org account"
   type        = string
-  default     = "devops@nurdsoft.co"
 }
