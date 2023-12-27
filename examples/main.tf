594cc079bf0f414a9823553a0fbda4a5674c4d3c
Arnav <arnavchachra.ac@gmail.com>
2023-12-27 20:54:17 +0530

    fix: changed email in examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module aws_organizations_account {
   source = "./terraform-aws-organizations/"
   account_name = "test01"
-  account_email = "982349823492834@test-01.com"
+  account_email = "982349823492834@982349823492834.com"
 }
