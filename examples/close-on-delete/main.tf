dfca952ff01f98f5bb88d3da8e6d0c0a3316428d
Arnav <arnavchachra.ac@gmail.com>
2024-02-05 20:19:51 +0530

    feat: added module outputs to examples

--- examples/close-on-delete/main.tf
+++ examples/close-on-delete/main.tf
@@ -4,3 +4,11 @@ module "aws_organizations_account" {
   account_email     = "devops@nurdsoft.co"
   close_on_deletion = "true"
 }
+
+output "new_account_arn" {
+  value = module.aws_organizations_account.account_arn
+}
+
+output "new_account_id" {
+  value = module.aws_organizations_account.account_id
+}
--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,3 +3,11 @@ module "aws_organizations_account" {
   account_name  = "aws-nurdsoft-demo"
   account_email = "devops@nurdsoft.co"
 }
+
+output "new_account_arn" {
+  value = module.aws_organizations_account.account_arn
+}
+
+output "new_account_id" {
+  value = module.aws_organizations_account.account_id
+}
